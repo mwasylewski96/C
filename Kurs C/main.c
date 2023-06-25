fac78b26f86ede9bb40df9ceb5e1963066216e6a
Marcin Wasylewski <m.wasylewski96@gmail.com>
2023-06-25 14:08:15 +0200

    C training

---
+++ Kurs C/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//int main()
+//{
+    //printf("Marcin Wasylewski\n");
+    //printf("27\n");
+    //printf("Male\n");
+//    printf("Marcin Wasylewski\n27\nMale\n");
+//    return 0;
+//}
