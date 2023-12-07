a98d940138f6eb7b978852e71cebcb03ce4a38c7
Marcin Wasylewski <m.wasylewski96@gmail.com>
2023-12-07 01:31:42 +0100

    C training 28.0

---
+++ Kurs C/Structs_4.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+/*
+typedef struct point{
+    int x;
+    int y;
+}POINT;
+
+POINT insertPointCoordinates();
+void initializeCoordinatesOfPointsArray(POINT *points);
+void printCoordinatesOfAllPointsInArrays(POINT *points);
+
+
+void main()
+{
+    POINT points[5];
+    initializeCoordinatesOfPointsArray(&points);
+    printCoordinatesOfAllPointsInArrays(&points);
+}
+
+
+POINT insertPointCoordinates()
+{
+    POINT pt;
+    printf("Insert x and y coordinate of point: \n");
+    scanf("%d %d", &pt.x, &pt.y);
+    return pt;
+}
+
+void initializeCoordinatesOfPointsArray(POINT *points)
+{
+    for (int i=0; i<5; i++)
+        points[i] = insertPointCoordinates();
+}
+
+
+void printCoordinatesOfAllPointsInArrays(POINT *points)
+{
+    for (int i=0; i<5; i++)
+        printf("Point %d: (%d,%d)\n", i, points[i].x, points[i].y);
+}
+*/
---
+++ Kurs C/Structs_5.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/*
+typedef struct point{
+    int x;
+    int y;
+}POINT;
+
+int compareTwoPoints(POINT pt1, POINT pt2)
+{
+    if ((pt1.x != pt2.x) || (pt1.y != pt2.y))
+        return 0;
+    return 1;
+}
+void main()
+{
+    POINT pt1 = {2,3}, pt2 = {2,3};
+    int result = compareTwoPoints(pt1, pt2);
+    printf("Result of comparision two points is %d", result);
+}
+*/
